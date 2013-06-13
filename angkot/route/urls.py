3cc510d2b2c2e7828ee425290e961e85e8775c80
Fajran Iman Rusadi <fajran@gmail.com>
2013-06-13 02:43:19 +0200

    Client side submission code

--- angkot/route/urls.py
+++ angkot/route/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('angkot.route.views',
     url(r'^$', 'index', name='route_index'),
     url(r'^(?P<route_id>[A-Za-z0-9]+)/$', 'editor', name='route_editor'),
+    url(r'^(?P<route_id>[A-Za-z0-9]+)/submit/$', 'submit', name='route_submit'),
 )
 
--- angkot/route/views.py
+++ angkot/route/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
+from django.views.decorators.csrf import requires_csrf_token, ensure_csrf_cookie
 
 from .utils import generate_route_id
+from .decorators import api
 
 def index(request):
     route_id = generate_route_id()
     return redirect('route_editor', route_id=route_id)
 
+@ensure_csrf_cookie
 def editor(request, route_id):
     return render_to_response('route/editor.html',
                               context_instance=RequestContext(request))
+
+@requires_csrf_token
+@api
+def submit(request, route_id):
+    return {}
 
