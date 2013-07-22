ccac5dad9817f5fa6bb98a6291a0e66eaa15b45b
Fajran Iman Rusadi <fajran@gmail.com>
2013-07-23 01:36:54 +0200

    Back to Google Maps

--- design.js
+++ design.js
@@ -4,21 +4,21 @@
 
 var JAKARTA = [-6.1744444, 106.8294444];
 
-var map = undefined;
+var gm = google.maps,
+    map = undefined;
 
 function setupMap() {
-  map = L.map('map', {
-    center: JAKARTA,
-    zoom: 13,
-    zoomControl: false,
-  })
+  var target = document.getElementById('map');
+  var opts = {
+    center: new gm.LatLng(JAKARTA[0], JAKARTA[1]),
+    zoom: 12,
+    minZoom: 11,
+    maxZoom: 18,
+    mapTypeId: gm.MapTypeId.ROADMAP,
+    streetViewControl: false,
+  }
 
-  L.tileLayer('http://a.tiles.mapbox.com/v3/'+MAPBOX_KEY+'/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://www.mapbox.com">MapBox</a>',
-    maxZoom: 18
-  }).addTo(map);
-
-  new L.Control.Zoom({position: 'bottomright'}).addTo(map);
+  map = new gm.Map(target, opts);
 }
 
 function setupPage() {
