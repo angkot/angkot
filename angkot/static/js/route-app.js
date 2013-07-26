18ec1d85a8638373a0cf4f14da108d2ccaeaae7f
Fajran Iman Rusadi <fajran@gmail.com>
2013-07-26 17:18:00 +0200

    Update interpolator, csrf token, and form submission format

--- angkot/static/js/route-app.js
+++ angkot/static/js/route-app.js
@@ -1,6 +1,17 @@
 (function(angkot) {
 
-angkot.route.app = angular.module('AngkotRouteEditor', []);
+var app = angular.module('AngkotRouteEditor', []);
+
+app.config(function($interpolateProvider, $httpProvider) {
+  $interpolateProvider.startSymbol('((');
+  $interpolateProvider.endSymbol('))');
+
+  // From: http://thomas.rabaix.net/blog/2013/05/csrf-token-security-with-angularjs
+  $httpProvider.defaults.headers.common['X-CSRFToken'] = jQuery('input[name=csrfmiddlewaretoken]').attr('value');
+  $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+});
+
+angkot.route.app = app;
 
 })(angkot);
 
