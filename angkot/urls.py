dd1283f2a4848fed881377d70ebe3e0ed42aa936
Fajran Iman Rusadi <fajran@gmail.com>
2013-07-27 02:00:02 +0200

    Redirect to the route page by default

--- angkot/urls.py
+++ angkot/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'angkot.views.home', name='home'),
     url(r'^route/', include('angkot.route.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/route/', permanent=False)),
 )
