0d0532384f54cfba91ddb13f25f5703495da697a
Fajran Iman Rusadi <fajran@gmail.com>
2013-07-30 15:08:08 +0200

    Show parse status in the admin page

--- angkot/route/admin.py
+++ angkot/route/admin.py
@@ -8,7 +8,7 @@ class SubmissionAdmin(admin.GeoModelAdmin):
     visitor_id.allow_tags = True
     visitor_id.admin_order_field = 'visitor_id'
 
-    list_display = ('submission_id', 'ip_address', visitor_id, 'created',)
+    list_display = ('submission_id', 'ip_address', visitor_id, 'parsed_ok', 'created',)
 
 admin.site.register(Submission, SubmissionAdmin)
 
