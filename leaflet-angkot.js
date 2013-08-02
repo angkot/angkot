a6cdb1a7fd70f9ac862c00b9d7fd9724a9a5d601
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-02 12:56:17 +0200

    Rewritten with shadow lines support
    Closes #1

--- leaflet-angkot.js
+++ leaflet-angkot.js
@@ -1,114 +1,3 @@
-L.Marker.Handle = L.Marker.extend({
-  options: {
-    type: 'vertex',
-  },
-
-  initialize: function(latlng, options) {
-    L.Marker.prototype.initialize.call(this, latlng, options);
-    L.Util.setOptions(this, options);
-
-    this._polyline = undefined;
-    this._index = 0;
-
-    this.on('dragstart', this._onDragStart, this);
-    this.on('drag', this._onDrag, this);
-    this.on('dragend', this._onDragEnd, this);
-
-    this.on('mouseover', this._onMouseEvent, this);
-    this.on('mouseout', this._onMouseEvent, this);
-    this.on('click', this._onMouseEvent, this);
-  },
-
-  onAdd: function() {
-    L.Marker.prototype.onAdd.apply(this, arguments);
-    this.fire('add');
-  },
-
-  setPolyline: function(polyline) {
-    this._polyline = polyline;
-  },
-
-  onAdd: function(map) {
-    L.Marker.prototype.onAdd.apply(this, arguments);
-
-    if (!this._polyline) return;
-
-    if (this.options.type === 'vertex') {
-      var index = this._polyline._handles.indexOf(this);
-      var pos = this._polyline._latlngs[index];
-      this.setLatLng(pos);
-    }
-    else {
-      var index = this._polyline._mids.indexOf(this);
-      var left = this._polyline._latlngs[index];
-      var right = this._polyline._latlngs[index+1];
-      var mid = this._getMidLatLng(left, right);
-      this.setLatLng(mid);
-    }
-  },
-
-  _getMidLatLng: function(a, b) {
-    var aa = this._map.latLngToLayerPoint(a);
-    var bb = this._map.latLngToLayerPoint(b);
-    return this._map.layerPointToLatLng(aa._add(bb)._divideBy(2));
-  },
-
-  _onDragStart: function(e) {
-    if (this.options.type === 'edge') {
-      var index = this._polyline._mids.indexOf(this);
-      this._polyline._handles.splice(index+1, 0, this);
-      this.options.type = 'vertex';
-      var className = this._icon.className;
-      this._icon.className = className.replace('leaflet-angkot-handle-edge', 'leaflet-angkot-handle-vertex');
-
-      var left = L.Polyline.Editable.handle(this._polyline, 'edge');
-      var right = L.Polyline.Editable.handle(this._polyline, 'edge');
-      this._polyline._mids.splice(index, 1, left, right);
-      this._polyline._latlngs.splice(index+1, 0, e.target._latlng);
-
-      left.addTo(this._map);
-      right.addTo(this._map);
-    }
-
-    this._index = this._polyline._handles.indexOf(this);
-    this._hasLeftEdge = this._index > 0;
-    this._hasRightEdge = this._index < this._polyline._handles.length-1;
-  },
-
-  _onDrag: function(e) {
-    var latlng = e.target._latlng;
-    this._polyline._latlngs[this._index] = latlng;
-    this._polyline.redraw();
-
-    if (this._hasLeftEdge) {
-      var left = this._polyline._latlngs[this._index-1];
-      var mid = this._getMidLatLng(left, latlng);
-      this._polyline._mids[this._index-1].setLatLng(mid);
-    }
-    if (this._hasRightEdge) {
-      var right = this._polyline._latlngs[this._index+1];
-      var mid = this._getMidLatLng(latlng, right);
-      this._polyline._mids[this._index].setLatLng(mid);
-    }
-  },
-
-  _onDragEnd: function(e) {
-  },
-
-  _onMouseEvent: function(e) {
-    if (!this._polyline) return;
-
-    if (this.options.type === 'vertex') {
-      e.vertex = this._polyline._handles.indexOf(this);
-    }
-    else if (this.options.type === 'edge') {
-      e.edge = this._polyline._mids.indexOf(this);
-    }
-
-    this._polyline.fire('handle:' + e.type, e);
-  },
-});
-
 L.Polyline.Editable = L.Polyline.extend({
   options: {
     editable: false,
@@ -119,7 +8,37 @@ L.Polyline.Editable = L.Polyline.extend({
     L.Util.setOptions(this, options);
 
     this._handles = [];
-    this._mids = [];
+    this._handleGroup = new L.LayerGroup();
+    this._shadow = new L.Polyline([], {
+      opacity: 0.3,
+      weight: 5,
+    });
+
+    this._resetHandles();
+  },
+
+  onAdd: function(map) {
+    L.Polyline.prototype.onAdd.apply(this, arguments);
+
+    this._updateHandlePositions(map);
+    this._shadow.addTo(map);
+    this._handleGroup.addTo(map);
+  },
+
+  onRemove: function(map) {
+    L.Polyline.prototype.onRemove.apply(this, arguments);
+
+    map.removeLayer(this._handleGroup);
+    map.removeLayer(this._shadow);
+  },
+
+  addLatLng: function(latlng) {
+    L.Polyline.prototype.addLatLng.apply(this, arguments);
+    if (this.options.editable) this._addHandle(latlng);
+  },
+
+  spliceLatLngs: function() {
+    L.Polyline.prototype.spliceLatLngs.apply(this, arguments);
     this._resetHandles();
   },
 
@@ -128,83 +47,162 @@ L.Polyline.Editable = L.Polyline.extend({
     this._resetHandles();
   },
 
-  addLatLng: function(latlng) {
-    L.Polyline.prototype.addLatLng.apply(this, arguments);
-    this._resetHandles(); // FIXME
-  },
-
-  spliceLatLngs: function() {
-    L.Polyline.prototype.spliceLatLngs.apply(this, arguments);
-    this._resetHandles(); // FIXME
-  },
-
-  onAdd: function(map) {
-    L.Polyline.prototype.onAdd.apply(this, arguments);
-    this._resetHandles(); // FIXME
-  },
-
-  onRemove: function(map) {
-    L.Polyline.prototype.onRemove.apply(this, arguments);
-    this._resetHandles(); // FIXME
-  },
-
   _resetHandles: function() {
-    if (this._map) {
-      for (var i=0; i<this._handles.length; i++) {
-        this._map.removeLayer(this._handles[i]);
-      }
-      for (var i=0; i<this._mids.length; i++) {
-        this._map.removeLayer(this._mids[i]);
-      }
+    for (var i=0; i<this._handles.length; i++) {
+      this._handleGroup.removeLayer(this._handles[i]);
     }
-
     this._handles.splice(0, this._handles.length);
-    this._mids.splice(0, this._mids.length);
 
     if (!this.options.editable) return;
 
-    var len = this._latlngs.length;
-    var prev = undefined;
-    for (var i=0; i<len; i++) {
-      var latlng = this._latlngs[i];
+    for (var i=0; i<this._latlngs.length; i++) {
+      this._addHandle(this._latlngs[i]);
+    }
+  },
 
-      if (prev) {
-        var h = L.Polyline.Editable.handle(this, 'edge');
-        this._mids.push(h);
-      }
-
-      var h = L.Polyline.Editable.handle(this, 'vertex');
-      this._handles.push(h);
-
-      prev = latlng;
+  _addHandle: function(latlng) {
+    var edge, vertex;
+    if (this._handles.length > 0) {
+      var edge = this._createHandle('edge');
+      edge.addTo(this._handleGroup);
+      this._handles.push(edge);
     }
 
+    var vertex = this._createHandle('vertex');
+    vertex.addTo(this._handleGroup);
+    this._handles.push(vertex);
+
     if (this._map) {
-      for (var i=0; i<this._handles.length; i++) {
-        this._handles[i].addTo(this._map);
+      if (edge) {
+        this._updateEdgeHandlePosition(edge);
       }
-      for (var i=0; i<this._mids.length; i++) {
-        this._mids[i].addTo(this._map);
+      vertex.setLatLng(latlng);
+    }
+  },
+
+  _updateEdgeHandlePosition: function(h) {
+    var index = this._handles.indexOf(h);
+    if (!this._map || index<0) return;
+
+    var prev = this._latlngs[parseInt(index/2)];
+    var next = this._latlngs[parseInt(index/2)+1];
+    var mid = this._getMidLatLng(this._map, prev, next);
+    h.setLatLng(mid);
+  },
+
+  _createHandle: function(type) {
+    var icon = L.divIcon({
+      className: 'leaflet-angkot-handle leaflet-angkot-handle-' + type,
+      iconSize: new L.Point(10, 10),
+    });
+    var opts = {
+      draggable: true,
+      icon: icon,
+      type: type,
+    }
+
+    var handle = new L.Marker([0,0], opts);
+
+    handle.on('dragstart', this._onHandleDragStart, this);
+    handle.on('drag', this._onHandleDrag, this);
+    handle.on('dragend', this._onHandleDragEnd, this);
+
+    handle.on('mouseover', this._onHandleMouseEvent, this);
+    handle.on('mouseout', this._onHandleMouseEvent, this);
+    handle.on('click', this._onHandleMouseEvent, this);
+
+    return handle;
+  },
+
+  _updateHandlePositions: function(map) {
+    for (var i=0; i<this._handles.length; i++) {
+      var h = this._handles[i];
+      if (h.options.type === 'vertex') {
+        h.setLatLng(this._latlngs[parseInt(i/2)]);
+      }
+      else {
+        var prev = this._latlngs[parseInt(i/2)];
+        var next = this._latlngs[parseInt(i/2)+1];
+        var mid = this._getMidLatLng(map, prev, next);
+        h.setLatLng(mid);
       }
     }
   },
 
+  _getMidLatLng: function(map, a, b) {
+    var aa = map.latLngToLayerPoint(a);
+    var bb = map.latLngToLayerPoint(b);
+    return map.layerPointToLatLng(aa._add(bb)._divideBy(2));
+  },
+
+  _onHandleDragStart: function(e) {
+    var h = e.target;
+
+    var d = 2;
+    if (h.options.type === 'edge') d = 1;
+
+    var p = [];
+    var index = this._handles.indexOf(h);
+    if (index > 0) {
+      p.push(this._handles[index-d]._latlng);
+    }
+    if (index < this._handles.length-1) {
+      p.push(this._handles[index+d]._latlng);
+    }
+    p.splice(1, 0, h._latlng);
+
+    this._shadow.setLatLngs(p);
+  },
+
+  _onHandleDrag: function(e) {
+    this._shadow.spliceLatLngs(1, 1, e.target._latlng);
+  },
+
+  _onHandleDragEnd: function(e) {
+    this._shadow.setLatLngs([]);
+
+    var h = e.target;
+    var index = this._handles.indexOf(h);
+
+    if (h.options.type === 'vertex') {
+      this._latlngs.splice(parseInt(index/2), 1, h._latlng);
+      this._updateEdgeHandlePosition(this._handles[index-1]);
+      this._updateEdgeHandlePosition(this._handles[index+1]);
+      this.redraw();
+    }
+    else {
+      this._latlngs.splice(parseInt(index/2)+1, 0, h._latlng);
+      h.options.type = 'vertex';
+      h._icon.className = h._icon.className.replace('leaflet-angkot-handle-edge', 'leaflet-angkot-handle-vertex');
+
+      var left = this._createHandle('edge');
+      var right = this._createHandle('edge');
+
+      this._handles.splice(index+1, 0, right);
+      this._handles.splice(index, 0, left);
+
+      if (this._map) {
+        left.addTo(this._map);
+        right.addTo(this._map);
+        this._updateEdgeHandlePosition(left);
+        this._updateEdgeHandlePosition(right);
+      }
+      this.redraw();
+    }
+  },
+
+  _onHandleMouseEvent: function(e) {
+    var h = e.target;
+
+    var index = parseInt(this._handles.indexOf(h)/2);
+    if (h.options.type === 'vertex') {
+      e.vertex = index;
+    }
+    else {
+      e.edge = index;
+    }
+
+    this.fire('handle:' + e.type, e);
+  },
+
 });
-
-L.Polyline.Editable.handle = function(polyline, type) {
-  var icon = L.divIcon({
-    className: 'leaflet-angkot-handle leaflet-angkot-handle-' + type,
-    iconSize: new L.Point(10, 10),
-  });
-  var opts = {
-    draggable: true,
-    icon: icon,
-    type: type,
-  }
-
-  var m = new L.Marker.Handle([0,0], opts);
-  m.setPolyline(polyline);
-
-  return m;
-};
-
