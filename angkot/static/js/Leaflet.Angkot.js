6b5209c87f86522760ea462aeb806a9235c30cfe
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-03 14:17:00 +0200

    Monkey patch L.Draggable:_onDrag
    Avoid shift click conflict
    
    Copyright (c) 2010-2013, Vladimir Agafonkin
    Copyright (c) 2010-2011, CloudMade
    All rights reserved.
    
    Redistribution and use in source and binary forms, with or without modification, are
    permitted provided that the following conditions are met:
    
       1. Redistributions of source code must retain the above copyright notice, this list of
          conditions and the following disclaimer.
    
       2. Redistributions in binary form must reproduce the above copyright notice, this list
          of conditions and the following disclaimer in the documentation and/or other materials
          provided with the distribution.
    
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
    TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

--- angkot/static/js/Leaflet.Angkot.js
+++ angkot/static/js/Leaflet.Angkot.js
@@ -1,2 +1,37 @@
 L.Angkot = {};
 
+// Taken and modified from Leaflet 0.6.4
+L.Draggable.include({
+  _onDown: function(e) {
+    // The following is commented for Angkot
+    // if (e.shiftKey || ((e.which !== 1) && (e.button !== 1) && !e.touches)) { return; }
+
+    L.DomEvent
+      .stopPropagation(e);
+
+    if (L.Draggable._disabled) { return; }
+
+    L.DomUtil.disableImageDrag();
+    L.DomUtil.disableTextSelection();
+
+    var first = e.touches ? e.touches[0] : e,
+        el = first.target;
+
+    // if touching a link, highlight it
+    if (L.Browser.touch && el.tagName.toLowerCase() === 'a') {
+      L.DomUtil.addClass(el, 'leaflet-active');
+    }
+
+    this._moved = false;
+
+    if (this._moving) { return; }
+
+    this._startPoint = new L.Point(first.clientX, first.clientY);
+    this._startPos = this._newPos = L.DomUtil.getPosition(this._element);
+
+    L.DomEvent
+        .on(document, L.Draggable.MOVE[e.type], this._onMove, this)
+        .on(document, L.Draggable.END[e.type], this._onUp, this);
+  },
+});
+
