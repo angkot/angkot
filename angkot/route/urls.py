4962fbf263cf3758ccb308243ab7307ba1fe5d4f
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-04 02:04:18 +0200

    Transportation data API

--- angkot/route/urls.py
+++ angkot/route/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('angkot.route.views',
     url(r'^submissions.json$', 'submission_list', name='route_submissions'),
     url(r'^provinces.json$', 'province_list', name='route_province_list'),
     url(r'^transportations.json$', 'transportation_list', name='route_transportation_list'),
+    url(r'^transportation/(?P<tid>\d+)\.json$',
+        'transportation_data', name='route_transportation_data'),
 )
 
