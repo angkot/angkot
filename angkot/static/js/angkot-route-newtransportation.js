fbcdbb915d6ccd195853764eb9b108747eea5189
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-11 18:51:42 +0200

    Update new transportation data form

--- angkot/static/js/angkot-route-newtransportation.js
+++ angkot/static/js/angkot-route-newtransportation.js
@@ -2,6 +2,78 @@
 
 app.controller('NewTransportationController', ['$scope', '$http', function($scope, $http) {
 
+  $scope.loading = 0;
+
+  $scope.saveCheck = function() {
+    $scope.error = null;
+    $scope.incomplete = null;
+
+    var valid = true;
+
+    var focus = function(name) {
+      jQuery('.new-transportation-form input[name="'+name+'"]').focus();
+    }
+
+    if (!$scope.province) {
+      focus('province');
+      valid = false;
+    }
+
+    if (valid && !$scope.city) {
+      focus('city');
+      valid = false;
+    }
+
+    if (valid && !$scope.number) {
+      focus('number');
+      valid = false;
+    }
+
+    if (valid && !$scope.termAgreement) {
+      focus('term-agreement');
+      valid = false;
+    }
+
+    $scope.incomplete = !valid;
+    $scope.checked = true;
+
+    if (valid) {
+      $scope.save();
+    }
+  }
+
+  $scope.save = function() {
+    $scope.loading++;
+
+    var data = {
+      province: $scope.province,
+      city: $scope.city,
+      number: $scope.number,
+    }
+    var url = jQuery('body').data('url-search-transportation') + '?' + jQuery.param(data);
+
+    $http.get(url)
+      .success(function(data) {
+        $scope.loading--;
+      });
+  }
+
+  $scope.$watch('loading', function(value, old) {
+    var btn = jQuery('.new-transportation-form button.btn.save');
+    if (old === 0 && value > 0) {
+      jQuery('.new-transportation-form input').attr('disabled', 'disabled');
+      jQuery('.new-transportation-form select').attr('disabled', 'disabled');
+      jQuery('.new-transportation-form button').attr('disabled', 'disabled');
+      btn.text('mengecek data..');
+    }
+    else if (value === 0 && old > 0) {
+      jQuery('.new-transportation-form input').removeAttr('disabled');
+      jQuery('.new-transportation-form select').removeAttr('disabled');
+      jQuery('.new-transportation-form button').removeAttr('disabled');
+      btn.text('Simpan');
+    }
+  });
+
 }]);
 
 })(window.angkot.app);
