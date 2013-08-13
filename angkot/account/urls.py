9853593a63e49f0aacda06e71b12fa43d0fbea2e
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-14 00:41:15 +0200

    Add account info API

--- angkot/account/urls.py
+++ angkot/account/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('angkot.account.views',
     url(r'^$', 'index', name='account_index'),
+    url(r'^info.json$', 'account_info', name='account_info'),
     url(r'^login/$', 'login_page', name='account_login_page'),
     url(r'^login/\+ok/$', 'login_success', name='account_login_success'),
     url(r'^logout/$', 'logout_page', name='account_logout_page'),
--- angkot/account/views.py
+++ angkot/account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 
-from angkot.decorators import login_required
+from angkot.decorators import api, login_required
 
 @login_required
 def index(request):
@@ -27,4 +27,14 @@ def logout_page(request):
     logout(request)
     return render_to_response('account/logout.html',
                               context_instance=RequestContext(request))
+
+@api
+def account_info(request):
+    res = {'authenticated': request.user.is_authenticated()}
+    if request.user.is_authenticated():
+        res['username'] = request.user.username
+        res['first_name'] = request.user.first_name
+        res['last_name'] = request.user.last_name
+
+    return res
 
