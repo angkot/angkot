887623c4d94d49d8278032c27b2864a63c8f39cd
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-14 22:06:31 +0200

    Add Bing Maps key settings

--- angkot/context_processors.py
+++ angkot/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 def configs(request):
     print 'blah'
     fields = ['GOOGLE_ANALYTICS_ID', 'GOOGLE_ANALYTICS_HOST',
-              'USERVOICE_CODE', 'MAPBOX_KEY',
+              'USERVOICE_CODE', 'MAPBOX_KEY', 'BING_MAPS_KEY',
               'ANGKOT_CONTRIBUTOR_TERMS_URL',
               'ANGKOT_PRIVACY_POLICY_URL',]
     return dict([(field, getattr(settings, field, None)) for field in fields])
