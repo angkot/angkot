82312173ddf74e1dd942566c075aef3134877ee3
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-14 10:50:01 +0200

    Link to privacy policy

--- angkot/context_processors.py
+++ angkot/context_processors.py
@@ -4,6 +4,7 @@ def configs(request):
     print 'blah'
     fields = ['GOOGLE_ANALYTICS_ID', 'GOOGLE_ANALYTICS_HOST',
               'USERVOICE_CODE', 'MAPBOX_KEY',
-              'ANGKOT_CONTRIBUTOR_TERMS_URL']
+              'ANGKOT_CONTRIBUTOR_TERMS_URL',
+              'ANGKOT_PRIVACY_POLICY_URL',]
     return dict([(field, getattr(settings, field, None)) for field in fields])
 
