4d8ffe5b19dd6637df06ac71f17d1d97339f76b9
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-15 20:56:25 +0200

    Settings flag to disable google analytics
    Instead of sending data to Google Analytics, log the data to console

--- angkot/context_processors.py
+++ angkot/context_processors.py
@@ -1,10 +1,17 @@
 from django.conf import settings
 
 def configs(request):
-    print 'blah'
     fields = ['GOOGLE_ANALYTICS_ID', 'GOOGLE_ANALYTICS_HOST',
+              'GOOGLE_ANALYTICS_IGNORE',
               'USERVOICE_CODE', 'MAPBOX_KEY', 'BING_MAPS_KEY',
               'ANGKOT_CONTRIBUTOR_TERMS_URL',
               'ANGKOT_PRIVACY_POLICY_URL',]
-    return dict([(field, getattr(settings, field, None)) for field in fields])
+    data = dict([(field, getattr(settings, field, None)) for field in fields])
+
+    fields = [('GOOGLE_ANALYTICS_IGNORE', False),]
+    data.update(dict([(field, getattr(settings, field, default))
+                      for field, default in fields
+                      if field not in data]))
+
+    return data
 
