bb757a13881ed4a44ad12121bb7aa8fe8ac67cc0
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-19 06:18:21 +0200

    Add packages.json and gruntfile.js
    Use Grunt to automate tasks. For now, only jshint

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['gruntfile.js', 'angkot/static/js/*.js'],
+      options: {
+        browser: true,
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true,
+          angular: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
