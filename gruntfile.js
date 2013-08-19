523e4f8fe9a7bd5b983cc90ecb84b680a93c054f
Fajran Iman Rusadi <fajran@gmail.com>
2013-08-19 06:33:53 +0200

    Minify javascripts - route page

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,42 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      route: {
+        src: [
+          'angkot/static/js/Leaflet.Tooltip.js',
+          'angkot/static/js/Leaflet.Control.TransportationInfo.js',
+          'angkot/static/js/Leaflet.Polyline.Editable.js',
+          'angkot/static/js/Leaflet.Angkot.js',
+          'angkot/static/js/Leaflet.Angkot.Route.js',
+          'angkot/static/js/Bing.js',
+          'angkot/static/js/modal.js',
+          'angkot/static/js/angkot-map.js',
+          'angkot/static/js/angkot-filter.js',
+          'angkot/static/js/angkot-route.js',
+          'angkot/static/js/angkot-route-menu.js',
+          'angkot/static/js/angkot-route-main.js',
+          'angkot/static/js/angkot-route-newtransportation.js',
+          'angkot/static/js/angkot-route-transportationlist.js',
+          'angkot/static/js/angkot-route-dataform.js',
+          'angkot/static/js/angkot-route-submissionlist.js'
+        ],
+        dest: 'dist/route.js'
+      }
+    },
+    uglify: {
+      route: {
+        options: {
+          banner: '/*! <%= pkg.name %> - route - <%= grunt.template.today("isoDateTime") %> */\n'
+        },
+        files: {
+          'dist/route.min.js': ['<%= concat.route.dest %>']
+        }
+      },
+    },
     jshint: {
       files: ['gruntfile.js', 'angkot/static/js/*.js'],
       options: {
@@ -17,13 +53,25 @@ module.exports = function(grunt) {
     },
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      tasks: ['jshint'/*, 'qunit'*/]
+    },
+    copy: {
+      js: {
+        files: [
+          {src: 'dist/route.min.js', dest: 'angkot/static/dist/js/route.min.js'}
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'copy']);
 
 };
