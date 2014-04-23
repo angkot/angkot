bca5fbc3f3c3fb4656fca98c20fd4f7a2ba2eb1c
Fajran Iman Rusadi <fajran@gmail.com>
2014-04-24 01:09:56 +0200

    Make gpolyencode works with Python 3
    also cleanup whitespaces and add helper function

--- angkot/common/utils/gpolyencode.py
+++ angkot/common/utils/gpolyencode.py
@@ -1,8 +1,5 @@
 import math
-try:
-    from cStringIO import StringIO
-except:
-    from StringIO import StringIO
+from io import StringIO
 
 class GPolyEncoder(object):
     def __init__(self, num_levels=18, zoom_factor=2, threshold=0.00001, force_endpoints=True):
@@ -10,11 +7,11 @@ class GPolyEncoder(object):
         self._zoom_factor = zoom_factor
         self._threshold = threshold
         self._force_endpoints = force_endpoints
-        
+
         self._zoom_level_breaks = []
         for i in range(num_levels):
             self._zoom_level_breaks.append(threshold * (zoom_factor ** (num_levels - i - 1)))
-    
+
     def encode(self, points):
         dists = {}
         # simplify using Douglas-Peucker
@@ -26,19 +23,19 @@ class GPolyEncoder(object):
             while len(stack):
                 current = stack.pop()
                 max_dist = 0
-                
+
                 for i in range(current[0]+1, current[1]):
                     temp = self._distance(points[i], points[current[0]], points[current[1]])
                     if temp > max_dist:
                         max_dist = temp
                         max_loc = i
                         abs_max_dist = max(abs_max_dist, max_dist)
-                
+
                 if max_dist > self._threshold:
                     dists[max_loc] = max_dist
                     stack.append((current[0], max_loc))
                     stack.append((max_loc, current[1]))
-        
+
         enc_points, enc_levels = self._encode(points, dists, abs_max_dist)
         r = {
             'points': enc_points,
@@ -47,7 +44,7 @@ class GPolyEncoder(object):
             'numLevels': self._num_levels,
         }
         return r
-    
+
     def _encode(self, points, dists, abs_max_dist):
         encoded_levels = StringIO()
         encoded_points = StringIO()
@@ -64,7 +61,7 @@ class GPolyEncoder(object):
         for i,p in enumerate(points):
             if (i > 0) and (i < n_points-1) and (i in dists):
                 encoded_levels.write(self._encode_number(self._num_levels - self._compute_level(dists[i]) -1))
-                
+
             if (i in dists) or (i == 0) or (i == n_points-1):
                 late5 = int(math.floor(p[1] * 1E5))
                 lnge5 = int(math.floor(p[0] * 1E5))
@@ -79,12 +76,12 @@ class GPolyEncoder(object):
             encoded_levels.write(self._encode_number(self._num_levels - 1))
         else:
             encoded_levels.write(self._encode_number(self._num_levels - self._compute_level(abs_max_dist) - 1))
-        
+
         return (
-            encoded_points.getvalue(), #.replace("\\", "\\\\"), 
+            encoded_points.getvalue(), #.replace("\\", "\\\\"),
             encoded_levels.getvalue()
         )
-    
+
     def _compute_level(self, abs_max_dist):
         lev = 0
         if abs_max_dist > self._threshold:
@@ -97,7 +94,7 @@ class GPolyEncoder(object):
         if num < 0:
             sgn_num = ~sgn_num
         return self._encode_number(sgn_num)
-    
+
     def _encode_number(self, num):
         s = StringIO()
         while num >= 0x20:
@@ -124,6 +121,10 @@ class GPolyEncoder(object):
             elif (0 < u) and (u < 1):
                 out = math.sqrt((p0[1] - p1[1] - u * (p2[1] - p1[1])) ** 2 \
                         + (p0[0] - p1[0] - u * (p2[0] - p1[0])) ** 2)
-        
+
         return out
+
+def encode(points):
+    encoder = GPolyEncoder()
+    return encoder.encode(points)
 
