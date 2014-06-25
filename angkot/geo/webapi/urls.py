860342ec466f593a4dada0b076e2079b0af0a66c
Fajran Iman Rusadi <fajran@gmail.com>
2014-06-25 02:41:38 +0200

    Add city list API

--- angkot/geo/webapi/urls.py
+++ angkot/geo/webapi/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('angkot.geo.webapi.views',
-    url(r'^provinces\.json$', 'province_list', name="wapi_province_list"),
+    url(r'^provinces\.json$', 'province_list', name="wapi_geo_province_list"),
+    url(r'^cities\.json$', 'city_list', name="wapi_geo_city_list"),
 )
 
--- angkot/geo/webapi/views.py
+++ angkot/geo/webapi/views.py
@@ -1,6 +1,6 @@
 from django.views.decorators.cache import cache_page
 
-from ..models import Province
+from ..models import Province, City
 
 from angkot.common.decorators import wapi
 
@@ -8,6 +8,12 @@ def _to_province(province):
     return dict(id=province.id,
                 name=province.name,
                 code=province.code)
+
+def _city_to_dict(city):
+    data = dict(cid=city.id,
+                name=city.name,
+                pid=city.province.id)
+    return (city.id, data)
 
 @cache_page(60 * 60 * 24)
 @wapi.endpoint
@@ -20,4 +26,27 @@ def province_list(req):
                                   .values_list('updated', flat=True)[0]
 
     return dict(provinces=provinces)
+
+@wapi.endpoint
+def city_list(req):
+    limit = 500
+    try:
+        page = int(req.GET.get('page', 0))
+    except ValueError:
+        page = 0
+
+    start = page * limit
+    end = start + limit
+    query = City.objects.filter(enabled=True) \
+                        .order_by('pk')
+
+    cities = query[start:end]
+    cities = dict(map(_city_to_dict, cities))
+
+    total = len(query)
+
+    return dict(cities=cities,
+                page=page,
+                count=len(cities),
+                total=total)
 
